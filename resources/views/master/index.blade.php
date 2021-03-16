74b72a69fbda449b9afa7fe7f76bb742ab0497ac
ilyasyasin072 <ilyasyasiin072@github.com>
2021-03-16 15:29:22 +0700

    add section, yield

--- resources/views/master/index.blade.php
+++ resources/views/master/index.blade.php
@@ -14,21 +14,23 @@
     @include('includes/navbar')
     <div class="container">
         <div class="col-xs-12 col-sm-12 col-md-12">
+            @yield('page_title')
             <div class="card-header">
             </div>
 
             <div class="card">
                 <div class="card-body">
-
+                    @yield('content')
                 </div>
             </div>
         </div>
     </div>
     {{-- FOOTER --}}
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-    </script>
+    @section('scripts')
+    <script src="assets/js/jquery.min.js"></script>
     <script src="assets/js/bootstrap.js"></script>
+    @stack('js')
+    @show
 </body>
 
 </html>
